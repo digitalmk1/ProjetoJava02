f3bca80ace019b53e9ed36a7298fb470564ed078
Suporte Digital MK <Suporte Digital MK@USER>
2021-07-29 17:23:13 -0300

    Ateração da classe pessoa adionando a variavel idade.

--- ProjetoJava02/src/Cadastros/Pessoa.java
+++ ProjetoJava02/src/Cadastros/Pessoa.java
@@ -1,5 +1,33 @@
 package Cadastros;
 
 public class Pessoa {
+	
+	private String nome;
+	private int idade;
+	
+	public int getIdade() {
+		return idade;
+	}
 
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	private String dataNascimento;
 }
