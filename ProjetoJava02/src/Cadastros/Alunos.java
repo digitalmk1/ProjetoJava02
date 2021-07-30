9296f0026b076ac0ea9a684b4ab8c6f62f7be14a
Suporte Digital MK <Suporte Digital MK@USER>
2021-07-30 20:03:25 -0300

    Adicionado a variavel Escola na classe aluno

--- ProjetoJava02/src/Cadastros/Alunos.java
+++ ProjetoJava02/src/Cadastros/Alunos.java
@@ -4,18 +4,19 @@ public class Alunos {
 
 	public static void main(String[] args) {
 		
-		String nome = "MARCIO OLIVEIRA";
+   String nome = "MARCIO OLIVEIRA";
+   String escola = "CLARICE FERREIRA";
 	int  nota1= 4;
     int nota2 =10;
-	int nota3 = 5;
+	int nota3 = 8;
 		
 	int resultado = (nota1 + nota2 + nota3)/3;
 	
 	int total = nota1+ nota2 + nota3;
 	//Você pode esta usando double no lugar de um intereiro no campo resultado
 		
-		if(total >=23) {
-			System.out.println("Aluno " + nome + " Aprovado com media "+  resultado + " Total de pontos "+ total);
+		if(total >=20) {
+			System.out.println("Aluno " + nome + "Escola:"+ escola  + " Aprovado com media "+  resultado + " Total de pontos "+ total);
 			  
 			
 			
