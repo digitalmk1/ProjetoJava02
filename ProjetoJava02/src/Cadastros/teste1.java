22c3f9ef6dbc3f6a6405b50cb346635dbd25c522
Suporte DigitalMk 02 <Suporte DigitalMk 02@DESKTOP-6B1S06H>
2021-07-30 11:15:53 -0300

    Teste com variaves

--- ProjetoJava02/src/Cadastros/teste1.java
+++ ProjetoJava02/src/Cadastros/teste1.java
@@ -5,17 +5,20 @@ public class teste1 {
 	public static void main(String[] args) {
 		
 	String nome = "Marcio silva de oliveira";
-	int ano = 2002;
-	int anoatual= 2021;
+	int idade1 = 25;
+	int idade2 = 20;
 	
-	int idade = (ano+anoatual);
+	int idade = idade1 + idade2;
 	
-	if(idade < 42) {
+
+	
+	if(idade > 42) {
 		
-		System.out.println("Apto para ser vacinado"+nome + idade);
+		System.out.println("Apto para ser vacinado"+ " "+ nome +" idade "+ idade +" Anos" );
 	} else {
 		
-		System.out.println("Aguarde a sua vez");
+		
+		System.out.println("Aguarde a sua vez"+ "idade atual " +idade+" Anos");
 	}
 	
 	
