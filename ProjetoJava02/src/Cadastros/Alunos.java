c8c1ad5cef2234512a85770ed097362c9cae5136
Suporte Digital MK <Suporte Digital MK@USER>
2021-07-31 20:18:16 -0300

    Operação aninhadas são operações dentro de operações

--- ProjetoJava02/src/Cadastros/Alunos.java
+++ ProjetoJava02/src/Cadastros/Alunos.java
@@ -12,40 +12,64 @@ public class Alunos {
 // Caso seja menor quero que esse aluno serja reprovado.
 
 		int nota1 = 80;
-		int nota2 = 60;
-		int nota3 = 40;
-		int nota4 = 75;
+		int nota2 = 80;
+		int nota3 = 70;
+		int nota4 = 10;
 		int media = 0;
 		String aluno = "Marcio";
 
-		// codição logica com IF E ELSE
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
-            /*
-		if (media == 60) {
 
-			System.out.println("Aluno " + aluno + " Aprovado com media " + media);
+		/* Operação aninhadas são operações dentro de operações */
 
-		} else if (media >= 40 && media <= 59) {
+		if (media >= 50) {
+			if (media >= 70) {
+				if(media >=80) {
+					
+					System.out.println("Aprovado com louvor");
+				}
 
-			System.out.println("Aluno " + aluno + " Em reucuperação com media " + media);
+				System.out.println(aluno + " Aluno Aprovado direto " + media);
+			} else {
 
-		}else if(media >=61 && media<=62) {
-			
-			System.out.println("Aluno quase se ferra Aprovado no conselho de classe com media" + media);
+				System.out.println("Aluno Em Recuperação!! " + media);
+			}
 		}else {
 			
-			System.out.println(" Aluno " + aluno + " Reprovado " + media);
-		} */
-              
-		
-		
-		         /*Operadores ternarios para micro validações com */
-		
-		String saidaResultado;
-		
-		saidaResultado = media >=60? "Aluno Aprovado":(media >= 40 && media <= 59) ? "Aluno em Recuperação": "Aluno reprovado";
-		
-		// nesse exemplo o else é representado por : coloca todos os codigos em 1 linha
-		System.out.println(saidaResultado +" " + media );
+		System.out.println("Aluno Reprovado direto ");
+		}
+
+		// codição logica com IF E ELSE
+
+		/*
+		 * if (media == 60) {
+		 * 
+		 * System.out.println("Aluno " + aluno + " Aprovado com media " + media);
+		 * 
+		 * } else if (media >= 40 && media <= 59) {
+		 * 
+		 * System.out.println("Aluno " + aluno + " Em reucuperação com media " + media);
+		 * 
+		 * }else if(media >=61 && media<=62) {
+		 * 
+		 * System.out.
+		 * println("Aluno quase se ferra Aprovado no conselho de classe com media" +
+		 * media); }else {
+		 * 
+		 * System.out.println(" Aluno " + aluno + " Reprovado " + media); }
+		 */
+
+		/* Operadores ternarios para micro validações com */
+
+		/*
+		 * String saidaResultado;
+		 * 
+		 * saidaResultado = media >=60? "Aluno Aprovado":(media >= 40 && media <= 59) ?
+		 * "Aluno em Recuperação": "Aluno reprovado";
+		 * 
+		 * // nesse exemplo o else é representado por : coloca todos os codigos em 1
+		 * linha System.out.println(saidaResultado +" " + media );
+		 */
+
 	}
 }
