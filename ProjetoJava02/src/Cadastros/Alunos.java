83679825a97a5b365b75c625dc5f1cda2befd2f5
Suporte Digital MK <Suporte Digital MK@USER>
2021-07-31 18:42:13 -0300

    Nota escolar com a condição logica IF e ELSE

--- ProjetoJava02/src/Cadastros/Alunos.java
+++ ProjetoJava02/src/Cadastros/Alunos.java
@@ -1,30 +1,40 @@
 package Cadastros;
 
+import com.sun.corba.se.spi.orbutil.fsm.State;
+
 public class Alunos {
 
+	static int mediaGeral = 10;
+
 	public static void main(String[] args) {
-		
-   String nome = "MARCIO OLIVEIRA";
-   String escola = "CLARICE FERREIRA";
-	int  nota1= 4;
-    int nota2 =10;
-	int nota3 = 8;
-		
-	int resultado = (nota1 + nota2 + nota3)/3;
-	
-	int total = nota1+ nota2 + nota3;
-	//Você pode esta usando double no lugar de um intereiro no campo resultado
-		
-		if(total >=20) {
-			System.out.println("Aluno " + nome + "Escola:"+ escola  + " Aprovado com media "+  resultado + " Total de pontos "+ total);
-			  
+// Para o aluno ser aprovado deve ter uma pontuação maior que 60 
+//para esta em recuperação o aluno deve ter uma nota entre 50 a 59
+// Caso seja menor quero que esse aluno serja reprovado.
+
+		int nota1 = 50;
+		int nota2 = 60;
+		int nota3 = 40;
+		int nota4 = 45;
+		int media = 0;
+		String aluno = "Marcio";
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media == 60) {
+
+			System.out.println("Aluno " + aluno + " Aprovado com media " + media);
+
+		} else if (media >= 40 && media <= 59) {
+
+			System.out.println("Aluno " + aluno + " Em reucuperação com media " + media);
+
+		}else if(media >=61 && media<=62) {
 			
-			
+			System.out.println("Aluno quase se ferra Aprovado no conselho de classe com media" + media);
 		}else {
 			
-			System.out.println("Aluno " + nome +" Reprovado com media " + resultado + " Total de pontos "+ total);
+			System.out.println(" Aluno " + aluno + " Reprovado " + media);
 		}
-		
-		
+
 	}
 }
