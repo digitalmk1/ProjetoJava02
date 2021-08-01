7b1748239d78bb588730c29eb7f5650719f4c80a
Suporte Digital MK <Suporte Digital MK@USER>
2021-08-01 11:52:47 -0300

    Fim do exercicio

--- ProjetoJava02/src/Cadastros/Exercicio_03_For.java
+++ ProjetoJava02/src/Cadastros/Exercicio_03_For.java
@@ -3,21 +3,31 @@ package Cadastros;
 import java.util.Iterator;
 
 public class Exercicio_03_For {
-	
+
 	public static void main(String[] args) {
-		
-		//String produto = "COCACOLA 3 LITROS ";
-		for(int numero=0; numero <=10;numero++){
-			if(numero ==7) {
-				
-				System.out.println( "Achei o numero  "+ numero);
-				
-				break;
-				
+
+		// String produto = "COCACOLA 3 LITROS ";
+		/*
+		 * for(int numero=0; numero <=10;numero++){ if(numero ==7) {
+		 * 
+		 * System.out.println( "Achei o numero  "+ numero);
+		 * 
+		 * break;
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+		String nome = "MARCIO";
+
+		for (int numero = 0; numero <= 20; numero++) {
+			if (numero == 7 || numero == 8 || numero == 10) {
+
+				System.out.println(nome + " 1'1'qaxAchei o numero  " + numero);
+
+				continue;
+
 			}
-			
-			
 		}
 	}
-
 }
