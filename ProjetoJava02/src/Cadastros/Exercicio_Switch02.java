182611e2d51180d6f50a1bdaaae1731379c13159
Suporte Digital MK <Suporte Digital MK@USER>
2021-07-31 22:58:44 -0300

    Exercicio Switch02

--- ProjetoJava02/src/Cadastros/Exercicio_Switch02.java
+++ ProjetoJava02/src/Cadastros/Exercicio_Switch02.java
@@ -3,8 +3,48 @@ package Cadastros;
 public class Exercicio_Switch02 {
 
 	public static void main(String[] args) {
+		
+		String dia = "7";
+		
+		
+		switch (dia) {
+		case "1":
+			System.out.println("DOMINGO");
+			break;
+		case "2":
+			System.out.println("SEGUNDA-FEIRA");
+			break;
+		case "3":
+			System.out.println("TERÇA-FEIRA");
+			break;
+		case "4":
+			System.out.println("QUARTA-FEIRA");
+			break;
+		case "5":
+			System.out.println("QUINTA-FEIRA");
+			break;
+		case "6":
+			System.out.println("SEXTA-FEIRA");
+	          System.out.println("Dia de congregar com Rudnei");
+	          break;
+		
+		case "7":
+			System.out.println("SABADO");
+			System.out.println("Dia de congregar com Daril");
+			break;
+		default:
+			System.out.println("Esse dia não existe no calendario");
+			break;
+			
+			
+		}
+		
+		
+	}   
+		
 
-		String carros = "parati";
+		
+		/*String carros = "parati";
 
 		switch (carros) {
 		case "hatch":
@@ -25,6 +65,8 @@ public class Exercicio_Switch02 {
 			System.out.println("Não trabalhamos com este tipo de automóvel aqui");
 			break;
 		}
-	}
+		*/
+		
 
+   
 }
