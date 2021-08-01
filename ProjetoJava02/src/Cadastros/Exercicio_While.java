ceaaba51856e7748937a00adf0fa913a5e8b7583
Suporte Digital MK <Suporte Digital MK@USER>
2021-08-01 10:19:11 -0300

    conhecendo o comando DO WHILE

---
+++ ProjetoJava02/src/Cadastros/Exercicio_While.java
@@ -0,0 +1,32 @@
+package Cadastros;
+
+public class Exercicio_While {
+	
+	public static void main(String[] args) {
+		
+		/*int numero = 0;
+		
+		while(numero <=3) {
+			numero++;
+			System.out.println("Contando " + numero);
+			
+			
+			
+		}
+		*/
+		
+		
+		/* no do while primero execulta e depois verifica*/
+		int numero =0;
+		do {
+			System.out.println("contando : " + numero);
+			numero++;
+		} while (numero<=60);
+		
+		
+		
+		
+		
+	}
+                
+}
