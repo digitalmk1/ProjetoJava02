12767901816c7a60d63e2734bf54ec0deaa1acba
Suporte Digital MK <Suporte Digital MK@USER>
2021-08-01 10:40:38 -0300

    Criação da Estrutura de repetição com FOR

---
+++ ProjetoJava02/src/Cadastros/Exercicio_03_For.java
@@ -0,0 +1,15 @@
+package Cadastros;
+
+import java.util.Iterator;
+
+public class Exercicio_03_For {
+	
+	public static void main(String[] args) {
+		
+		for(int numero=10; numero >=0;numero--){
+			
+			System.out.println("contando : "+ numero);
+		}
+	}
+
+}
